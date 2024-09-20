560e7430a804f1e2862a5c4feaf1597921952388
daan <idaany@gmail.com>
2024-09-20 13:11:29 +0200

    Oops, forgot a file...

---
+++ src/ofxSimpleAppConstants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// To get OF constants
+//#include "ofConstants.h"
+
+// This file defines constants that are not subject to be changed.
+
+#define ofxSA_XML_ENGINE_PUGIXML 1
+#define ofxSA_XML_ENGINE_TINYXML 2
+
+#define ofxSA_GUI_THEME_DARK 0
+#define ofxSA_GUI_THEME_LIGHT 1
+#define ofxSA_GUI_THEME_CUSTOM 2
+
